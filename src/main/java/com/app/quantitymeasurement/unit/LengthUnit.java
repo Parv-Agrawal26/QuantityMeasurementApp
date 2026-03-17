c19b9161b22b00ade6b14cdd3214f681eb96d6d8
Parv Agrawal <agrawalparv262004@gmail.com>
2026-03-17 14:55:52 +0530

    [Parv Agrawal] Refactor.refactored UC16 with proper jdbc with mysql integration

---
+++ src/main/java/com/app/quantitymeasurement/unit/LengthUnit.java
@@ -0,0 +1,40 @@
+package com.app.quantitymeasurement.unit;
+
+public enum LengthUnit implements IMeasurable {
+
+    FEET(1.0),
+    INCH(1.0 / 12.0),
+    YARDS(3.0),
+    CENTIMETERS(1.0 / 12.0 * 0.393701);
+
+    private final double conversionFactor;
+
+    LengthUnit(double conversionFactor) {
+        this.conversionFactor = conversionFactor;
+    }
+
+    @Override
+    public double getConversionFactor() {
+        return conversionFactor;
+    }
+
+    @Override
+    public double convertToBaseUnit(double value) {
+        return value * conversionFactor;
+    }
+
+    @Override
+    public double convertFromBaseUnit(double baseValue) {
+        return baseValue / conversionFactor;
+    }
+
+    @Override
+    public String getUnitName() {
+        return name();
+    }
+
+    @Override
+    public String getMeasurementType() {
+        return "LENGTH";
+    }
+}
